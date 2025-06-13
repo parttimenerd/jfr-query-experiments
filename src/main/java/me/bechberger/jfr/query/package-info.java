bab63076a1f8943a1dfe36b8c3f0e74b8b27837c
Johannes Bechberger <johannes.bechberger@sap.com>
2025-06-13 16:03:31 +0200

    Initial commit

---
+++ src/main/java/me/bechberger/jfr/query/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The code in this package is mostly a (slight modified) version the OpenJDK classes
+ * of the same name.
+ */
+package me.bechberger.jfr.query;
---
+++ src/main/java/me/bechberger/jfr/tool/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The code is this package is a rewritten version of the JFR CLI classes.
+ */
+package me.bechberger.jfr.tool;
